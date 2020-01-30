ce6ea5678dd8930d94f61602a3796343a60fedf2
Elliott Groves <sevorge@gmail.com>
2020-01-30 00:48:13 -0500

    Initial commit.

---
+++ src/components/home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="home d-flex">
+		<img :src="require('@/assets/images/E.svg')">
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'home',
+}
+</script>
+
+<style scoped lang="scss">
+.home {
+	background: #000;
+}
+</style>
