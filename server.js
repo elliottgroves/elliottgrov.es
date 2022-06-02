d59c0696dc07e0d325a2f1fd6932db468acecd86
Elliott Groves <sevorge@gmail.com>
2022-06-02 08:07:58 -0400

    Vue history mode express middleware.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const serveStatic = require("serve-static")
+const serveStatic = require('serve-static');
+const history = require('connect-history-api-fallback');
 const path = require('path');
-app = express();
+const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(history());
 const port = process.env.PORT || 80;
 app.listen(port);
