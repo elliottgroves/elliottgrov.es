99096969eac69c49fc411460f9115fb8d79aa019
Elliott Groves <sevorge@gmail.com>
2025-01-23 19:43:21 -0500

    2025 v1 update.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useState } from 'react';
 import { HashLink } from 'react-router-hash-link';
 import { motion, useInView } from 'motion/react';
 import Splt from 'react-spltjs';
@@ -10,32 +10,63 @@ import WelcomeCube from './images/welcomecube.mp4';
 import Tuftress from './images/tuftress.mp4';
 import LGE from './images/lge.mp4';
 import Logo from './images/logo.svg';
+import ContactSplash from './images/contact_splash.svg';
 import './App.css';
 import './base.css';
 
 export default function App() {
+  const [scrolled, setScrolled] = useState(false);
+
+  const scrollWithOffset = (el, offset) => {
+    const elementPosition = el.offsetTop - offset;
+    window.scroll({
+      top: elementPosition,
+      left: 0,
+      behavior: "smooth"
+    });    
+  }
+
   return (
     <>
-      <motion.nav id="main-nav">
-        <HashLink to="/#" smooth className="logo-home-link">
+      <motion.div id="scrolled-toggle"
+        viewport={{amount: 'all'}}
+        onViewportEnter={() => { setScrolled(false); }}
+        onviewportLeave={() => { setScrolled(true); }}
+      />
+      <motion.nav id="main-nav" className={scrolled ? 'scrolled' : ''}>
+        <span>{scrolled}</span>
+        <HashLink 
+          to="/#" 
+          smooth
+          className="logo-home-link"
+        >
           <img src={Logo} alt="Elliott Groves Design logo" class="logo"/>
           <h1>Elliott Groves Design.</h1>
         </HashLink>
         <ul>
           <li>
-            <HashLink to="#sites" smooth>
+            <HashLink 
+              to="#sites" 
+              scroll={el => scrollWithOffset(el, 0)}
+            >
               <Browsers />
               <span>Sites</span>
             </HashLink>
           </li>
           <li>
-            <HashLink to="#work" smooth>
+            <HashLink 
+              to="#work" 
+              scroll={el => scrollWithOffset(el, 0)}
+            >
               <ReadCvLogo />
               <span>Work</span>
             </HashLink>
           </li>
           <li>
-            <HashLink to="#contact" smooth>
+            <HashLink 
+              to="#contact" 
+              scroll={el => scrollWithOffset(el, 0)}
+            >
               <AddressBook />
               <span>Contact</span>
             </HashLink>
@@ -47,8 +78,6 @@ export default function App() {
       </main>
       <footer id="main-footer">
         <h1>Elliott Groves Design.</h1>
-        <p>
-        </p>
         <p>Thanks for visiting! This site was built using <a href="https://react.dev/" target="_blank" rel="noreferrer">React</a> and <a href="https://motion.dev/" target="_blank" rel="noreferrer">Motion</a>. It also uses <a href="https://phosphoricons.com/" target="_blank" rel="noreferrer">Phosphor Icons</a>.</p>
         <p className="copyright"><Copyright/> 2025 Elliott Groves Design</p>
       </footer>
@@ -57,24 +86,14 @@ export default function App() {
 }
 
 function HomePage() {
-  const listItemWithVideo = useRef(null);
-  const isInView = useInView(listItemWithVideo, { amount: 0.2 });
-
-  useEffect(() => {
-    if (isInView) {
-      listItemWithVideo.current.play();
-    } else {
-      listItemWithVideo.current.pause();
-    }
-  }, [isInView]);
-
-
   const listItemWithVideoVariants = {
     onscreen: {
-      background: 'green'
+      transformOrigin: 'top left',
+      scale: 1
     },
     offscreen: {
-      background: 'transparent'
+      transformOrigin: 'top left',
+      scale: 0.8
     }
   }
 
@@ -115,12 +134,18 @@ function HomePage() {
   return (
     <>
       <section className="hero">
-        <h2>
-          <Splt text="Your partner for"/>
+        <h2 aria-label="Your partner for beautiful, seamless web experiences.">
+          <span aria-hidden="true">
+            <Splt text="Your partner for" speed={0.2} offset={0.05} ease="ease-in" />
+          </span>
           <br/>
-          <em><Splt text="beautiful, seamless"/></em>
+          <em aria-hidden="true">
+            <Splt text="beautiful, seamless" speed={0.6} offset={0.05} ease="ease-in"/>
+          </em>
           <br/>
-          <Splt text="web experiences."/>
+          <span aria-hidden="true">
+            <Splt text="web experiences." speed={1.0} offset={0.05} ease="ease-in"/>
+          </span>
         </h2>
         <img src={Headshot} alt="A picture of Elliott" className="headshot"/>
       </section>
@@ -131,7 +156,7 @@ function HomePage() {
             <motion.li
               initial="offscreen"
               whileInView="onscreen"
-              viewport={{amount: 'all'}}
+              viewport={{amount: 0.8}}
             >
               <motion.div variants={listItemWithVideoVariants}>
                 <motion.video
@@ -148,17 +173,10 @@ function HomePage() {
               </motion.div>
             </motion.li>
           ))}
-          <motion.li initial="offscreen" whileInView="onscreen" viewport={{amount: 0.5}}>
-            <motion.div variants={listItemWithVideoVariants}>
-              <motion.video ref={listItemWithVideo} src={SomeGuys} loop muted playsInline/>
-              <a href="http://someguyspizza.com" target="_blank" rel="noreferrer">Some Guys Pizza</a>
-              <p>Business and menu site for a local relaxed Italian eatery.</p>
-            </motion.div>
-          </motion.li>
         </ul>
       </section>
       <section id="work" className="resume">
-        <h2>Other Work History</h2>
+        <h2>Work History</h2>
         <ul>
           <li>
             <p className="date">2021-2023</p> 
@@ -175,6 +193,8 @@ function HomePage() {
         </ul>
       </section>
       <section id="contact" className="contact-info">
+        <img src={ContactSplash} alt="Contact splash graphic" className="contact-splash"/>
+        <h2>Contact Me</h2>
         <p className="card">If you're interested in a website for your small business, a portfolio site, or looking to hire an experienced frontend developer or UX designer, email me at [sevorge at gmail dot com] or connect with me <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/elliott-groves-130b8196/">on LinkedIn</a>!</p>
       </section>
     </>
